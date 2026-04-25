a6f3444e16b1d1212f71d9cc5d846717e1f3380b
Ven Corwell <vencorwell@gmail.com>
2026-04-25 16:38:18 -0400

    feat: created HashMap for users to be stored

--- src/main/java/edu/secourse/salonapp/services/UserService.java
+++ src/main/java/edu/secourse/salonapp/services/UserService.java
@@ -1,4 +1,22 @@
 package edu.secourse.salonapp.services;
 
-public class UserService {
+import edu.secourse.salonapp.models.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService
+{
+     // CRUD (Create Read Update Delete)
+
+    /*
+        Steph I made a hashmap to store the Users, that way the key can be the unique account number.
+        Feel free to change it if you feel it doesn't make sense, and we should stick to an array.
+        - Ven
+     */
+
+    private Map<Integer, User> users = new HashMap<Integer, User>();
+    private int userId = 0;
+
+
 }
